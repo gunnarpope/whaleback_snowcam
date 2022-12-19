3d95c9b26fca896125e34d1193c90757947e8569
Gunnar Pope <gunnarpope@gmail.com>
2022-12-18 21:38:22 -0500

    reorganizing

--- run_snowcam.sh
+++ run_snowcam.sh
@@ -1,5 +1,4 @@
 echo 'Images sent to  "http://localhost:8081/snowcam"'
 echo 'Image Files sent to fig/whaleback_snowcam.jpg'
 echo 'Streaming snowcam pictures....'
-motion -c motion.cfg -l log/motion.log 
-# sudo motion -l log/motion.log # don't run as root!
+motion -c snowcam/motion.cfg -l log/motion.log 
--- scripts/upload_img_s3.py
+++ scripts/upload_img_s3.py
@@ -29,8 +29,9 @@ if __name__ == "__main__":
     # Create an S3 client
     s3 = boto3.client('s3')
     
-    filename = 'fig/whaleback_snowcam.jpg'
+    filename = 'snowcam/fig/whaleback_snowcam.jpg'
     bucket_name = 'whaleback.snowcam'
+    object_name = 'fig/whaleback_snowcam.jpg' 
     
     # Uploads the given file using a managed uploader, which will split up large
     # files automatically and upload parts in parallel.
